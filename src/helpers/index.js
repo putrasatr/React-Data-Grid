d2d123c28be67a50daf1795326101bb08d1f3c10
putrasatr <satriaputrapratama2002@gmail.com>
2021-09-14 16:59:09 +0700

    add depedencies

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import { useLayoutEffect, useState } from "react";
+import namor from 'namor'
 
 export function useWindowSize() {
     const [size, setSize] = useState([0, 0]);
@@ -11,4 +12,37 @@ export function useWindowSize() {
         return () => window.removeEventListener('resize', updateSize);
     }, []);
     return size;
+}
+
+const range = len => {
+    const arr = []
+    for (let i = 0; i < len; i++) {
+        arr.push(i)
+    }
+    return arr
+}
+
+const newData = () => {
+    return {
+        firstName: namor.generate({ words: 1, numbers: 0 }),
+        lastName: namor.generate({ words: 1, numbers: 0 }),
+        age: Math.floor(Math.random() * 30),
+        visits: Math.floor(Math.random() * 100),
+        status: Math.random() > 0.4 ? "Relationship" : Math.random() > 0.65 ? "Not Relationship" : "Complicated",
+        progress: Math.floor(Math.random() * 100)
+    }
+}
+
+export const makeData = (...lens) => {
+    const makeDataLevel = (depth = 0) => {
+        const len = lens[depth]
+        return range(len).map(d => {
+            return {
+                ...newData(),
+                subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+            }
+        })
+    }
+
+    return makeDataLevel()
 }
