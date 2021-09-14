21fccd8795b68ebd48d258e4d1bbd6d1c8da76ca
putrasatr <satriaputrapratama2002@gmail.com>
2021-09-14 16:58:50 +0700

    Row Span Table

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { useWindowSize } from "../helpers";
-import NFLWP from "../assets/images/nfl.jpg"
+import { VStack, Image, Box, Flex } from "@chakra-ui/react";
+
+import ItemList from "./itemList";
+import RowSpanTable from "./rowSpanTable";
+import NFLWP from "../assets/images/nfl.jpg";
+import "./styles.css";
 
 export default function MyApp() {
-    const [width, height] = useWindowSize()
     return (
-        <div className="min-h-screen bg-red-100">
-            <div>
-                <img src={NFLWP} alt="" className="h-20 lg:h-100" />
-            </div>
-        </div>
+        <VStack width="100%">
+            <span>React Table Example</span>
+            <Image src={NFLWP} height="40%" />
+            <VStack w="100%">
+                <Flex>
+                    <Box w="100%" bg="gray">
+                        <ItemList />
+                    </Box>
+                </Flex>
+            </VStack>
+            <Image src={NFLWP} height="40%" />
+            <RowSpanTable />
+        </VStack>
     )
 }
---
+++ src/components/rowSpanTable.js
@@ -0,0 +1,153 @@
+import * as React from "react";
+import { useTable } from "react-table";
+
+const borderStyle = {
+    border: "1px solid gray",
+    padding: "8px 10px"
+};
+
+function useInstance(instance) {
+    const { allColumns } = instance;
+
+    let rowSpanHeaders = [];
+
+    allColumns.forEach((column, i) => {
+        const { id, enableRowSpan } = column;
+
+        if (enableRowSpan !== undefined) {
+            rowSpanHeaders = [
+                ...rowSpanHeaders,
+                { id, topCellValue: null, topCellIndex: 0 }
+            ];
+        }
+    });
+
+    Object.assign(instance, { rowSpanHeaders });
+}
+
+export default function App() {
+    const origData = [
+        {
+            actor: "Johnny Depp",
+            movies: [
+                {
+                    name: "Pirates of the Carribean 1"
+                },
+                {
+                    name: "Pirates of the Carribean 2"
+                }
+            ]
+        },
+        {
+            actor: "Daniel Craig",
+            movies: []
+        },
+        {
+            actor: "Reza Rahardian",
+            movies: [
+                { name: "My Stupid Boss" }
+            ]
+        }
+    ];
+    const newData = [];
+    origData.forEach(actorObj => {
+        if (actorObj.movies.length)
+            return actorObj.movies.forEach(movie => {
+                newData.push({
+                    actor: actorObj.actor,
+                    movie: movie.name
+                });
+            });
+        newData.push({
+            actor: actorObj.actor,
+            movie: ""
+        });
+    });
+    const data = React.useMemo(() => newData, []);
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "Actor",
+                accessor: "actor",
+                enableRowSpan: true
+            },
+            {
+                Header: "Movies",
+                accessor: "movie"
+            }
+        ],
+        []
+    );
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        rowSpanHeaders
+    } = useTable({ columns, data }, hooks => {
+        hooks.useInstance.push(useInstance);
+    });
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()} style={borderStyle}>
+                                {column.render("Header")}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row);
+
+                    for (let j = 0; j < row.allCells.length; j++) {
+                        let cell = row.allCells[j];
+                        let rowSpanHeader = rowSpanHeaders.find(
+                            x => x.id === cell.column.id
+                        );
+                        console.log(rowSpanHeader, cell)
+                        if (rowSpanHeader !== undefined) {
+                            if (
+                                rowSpanHeader.topCellValue === null ||
+                                rowSpanHeader.topCellValue !== cell.value
+                            ) {
+                                cell.isRowSpanned = false;
+                                rowSpanHeader.topCellValue = cell.value;
+                                rowSpanHeader.topCellIndex = i;
+                                cell.rowSpan = 1;
+                            } else {
+                                rows[rowSpanHeader.topCellIndex].allCells[j].rowSpan++;
+                                cell.isRowSpanned = true;
+                            }
+                        }
+                    }
+                    return null;
+                })}
+                {rows.map(row => {
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                if (cell.isRowSpanned) return null;
+                                else
+                                    return (
+                                        <td
+                                            style={borderStyle}
+                                            rowSpan={cell.rowSpan}
+                                            {...cell.getCellProps()}
+                                        >
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
