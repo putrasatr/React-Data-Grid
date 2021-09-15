2910de755197752da07e542000d0f1e967e4d0ca
putrasatr <satriaputrapratama2002@gmail.com>
2021-09-15 16:51:33 +0700

    Table drag and drop react table

---
+++ src/components/TableDnd.js
@@ -0,0 +1,37 @@
+import React, { useState, useMemo } from "react";
+import ReactTableDragColumnRow from "./dragableTable";
+
+import { makeData } from "../helpers"
+import "./styles.css";
+
+function App() {
+    let [data, setData] = useState({
+        heads: ["a", "b", "c", "d", "e", "f"],
+        rows: [
+            [1, 2, 3, 1, 2, 3],
+            [11, 2, 3, 11, 2, 3],
+            [1, 22, 3, 1, 22, 3],
+            [1, 2, 33, 1, 2, 33]
+        ]
+    });
+    return (
+        <div>
+            <span>react table drag column row</span>
+            <ReactTableDragColumnRow
+                heads={data.heads}
+                rows={data.rows}
+                onDragEnd={(type, from, to, newData) => {
+                    console.log({
+                        type,
+                        from,
+                        to,
+                        newData
+                    });
+                    setData(newData);
+                }}
+            />
+        </div>
+    );
+}
+
+export default App
