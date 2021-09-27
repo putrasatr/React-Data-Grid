63b14380d803934ecfc3ebd1e84c190400a09190
putrasatr <satriaputrapratama2002@gmail.com>
2021-09-27 17:19:05 +0700

    Button icon and status created

---
+++ src/components/ButtonIcon/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ButtonIcon() {
+    return (
+        <div>
+            <button>Icon</button>
+        </div>
+    )
+}
---
+++ src/components/Status/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Active = () => (
+    <div>Active</div>
+)
+export const NotActive = () => (
+    <div>Not Active</div>
+)
