122b76fa23dea72c9e9fa9ead4546cbbbd548d75
putrasatr <satriaputrapratama2002@gmail.com>
2021-10-28 16:16:33 +0700

    component new

--- src/components/index.js
+++ src/components/index.js
@@ -1,37 +1,77 @@
 import React from "react";
-import { VStack, Image, Box, Flex } from "@chakra-ui/react";
+import { Image, Box, Link, Text } from "@chakra-ui/react";
 
-import ItemList from "./itemList";
-import RowSpanTable from "./rowSpanTable";
-import NFLWP from "../assets/images/nfl.jpg";
-import TableDnd from "./TableDnd";
-import TableDndColumn from "./TableDndColumn";
-import Counter from "./counter";
-import EvergreenUI from "./Evergreen/evergreenUI";
-import TableSuit from "./Tablesuit";
+// import ItemList from "./itemList";
+// import RowSpanTable from "./rowSpanTable";
+// import NFLWP from "../assets/images/nfl.jpg";
+// import TableDnd from "./TableDnd";
+// import TableDndColumn from "./TableDndColumn";
+// import Counter from "./counter";
+// import EvergreenUI from "./Evergreen/evergreenUI";
+// import TableSuit from "./Tablesuit";
 import "./styles.css";
 
-export default function MyApp() {
+// export default function MyApp() {
+//   return (
+//     <VStack width="100%">
+//       <span>React Table Example</span>
+//       <Image src={NFLWP} height="40%" />
+//       <VStack w="100%">
+//         <Flex>
+//           <Box w="100%" bg="gray">
+//             <ItemList />
+//           </Box>
+//         </Flex>
+//       </VStack>
+//       <Image src={NFLWP} height="40%" />
+//       <RowSpanTable />
+//       <TableDnd />
+//       <span>Table drag and drop</span>
+//       <TableDndColumn />
+//       <span>Table With Pagination</span>
+//       <Counter />
+//       <EvergreenUI />
+//       <TableSuit />
+//     </VStack>
+//   );
+// }
+
+export default function App() {
   return (
-    <VStack width="100%">
-      <span>React Table Example</span>
-      <Image src={NFLWP} height="40%" />
-      <VStack w="100%">
-        <Flex>
-          <Box w="100%" bg="gray">
-            <ItemList />
-          </Box>
-        </Flex>
-      </VStack>
-      <Image src={NFLWP} height="40%" />
-      <RowSpanTable />
-      <TableDnd />
-      <span>Table drag and drop</span>
-      <TableDndColumn />
-      <span>Table With Pagination</span>
-      <Counter />
-      <EvergreenUI />
-      <TableSuit />
-    </VStack>
+    <Box p={4} display={{ md: "flex" }}>
+      <Box flexShrink={0}>
+        <Image
+          borderRadius="lg"
+          width={{ md: 40 }}
+          src="https://bit.ly/2jYM25F"
+          alt="Woman paying for a purchase"
+        />
+      </Box>
+      <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+        <Text
+          fontWeight="bold"
+          textTransform="uppercase"
+          fontSize="sm"
+          letterSpacing="wide"
+          color="teal.600"
+        >
+          Marketing
+        </Text>
+        <Link
+          mt={1}
+          display="block"
+          fontSize="lg"
+          lineHeight="normal"
+          fontWeight="semibold"
+          href="#"
+        >
+          Finding customers for your new business
+        </Link>
+        <Text mt={2} color="gray.500">
+          Getting a new business off the ground is a lot of hard work. Here are
+          five ideas you can use to find your first customers.
+        </Text>
+      </Box>
+    </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+// 2. Update the breakpoints as key-value pairs
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+});
+// 3. Extend the theme
+const theme = extendTheme({ breakpoints });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
